b45507d571b63ac481fc399e6111d0450292707f
altisha <altishapy@gmail.com>
2021-03-03 16:32:12 +0600

    password reset

--- inout/settings.py
+++ inout/settings.py
@@ -122,3 +122,6 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = 'home'
 
 LOGOUT_REDIRECT_URL = 'home'
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+EMAIL_FILE_PATH = str(BASE_DIR.joinpath('sent_emails'))
+
